2794c54485297490ca9d9d877b90e29e7ac61cbf
HeitorKHS <heitorkhs@gmail.com>
2023-04-20 16:28:31 -0300

    Exercicio 5

---
+++ Exercicio05.php
@@ -0,0 +1,12 @@
+<?php
+
+    echo "Exercício 5) Faça  um  programa  em PHP,  que  dado  o  valor  de  um  produto  vendido  por  um  
+    estabelecimento comercial, aplique um desconto de 35% sobre o valor do produto.
+     Por fim, informe como resultado o valor do produto com esse desconto.<br><br>";
+
+    $valor = 100;
+    $desconto = 100*0.70;
+
+    echo "Valor com desconto: {$desconto}";
+
+?>
