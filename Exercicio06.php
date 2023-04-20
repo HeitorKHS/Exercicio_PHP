fb012c69087403e43a0281b78875515f25a5fa02
HeitorKHS <heitorkhs@gmail.com>
2023-04-20 16:28:47 -0300

    Exercicio 6

---
+++ Exercicio06.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Exercício 6) Faça um programa em PHP, que dado três números inteiros n1, n2 e n3. 
+    Verifique e mostre qual é o maior deles.<br><br>";
+
+    $n1 = 100;
+    $n2 = 103;
+    $n3 = 101;
+
+    $maior = max($n1,$n2,$n3);
+
+    echo "O maior numero é {$maior}";
+
+?>
